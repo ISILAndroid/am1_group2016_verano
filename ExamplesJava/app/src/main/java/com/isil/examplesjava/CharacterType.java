7aa42847c5c9252aea4b409ecfcda65a22b9558f
Eduardo Medina <em@osp.pe>
2016-01-05 18:10:22 -0500

    charactertype

---
+++ ExamplesJava/app/src/main/java/com/isil/examplesjava/CharacterType.java
@@ -0,0 +1,35 @@
+package com.isil.examplesjava;
+
+/**
+ * Created by emedinaa on 5/01/16.
+ */
+public class CharacterType {
+
+    char symbol = 'a';
+    char symbolB = 'b';
+    char symbolA = 'A';
+    char symbolU = 'ù';
+
+    /*
+    char symbol = 'a';
+    writeLine("The code of '{0}' is: {1}",
+              symbol, (int) symbol);
+
+    symbol = 'b';
+    writeLine("The code of '{0}' is: {1}",
+              symbol, (int) symbol);
+
+    symbol = 'A';
+    writeLine("The code of '{0}' is: {1}",
+              symbol, (int) symbol);
+
+    symbol = 'ù'; // Cyrillic letter 'sht'
+    writeLine("The code of '{0}' is: {1}",
+              symbol, (int) symbol);
+     */
+
+    public String getMessage()
+    {
+        return "";
+    }
+}
